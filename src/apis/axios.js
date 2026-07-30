b48535455e9b6434ad1ad7c6007d788b9b4ea580
Muhammad Usman <abc.usman.100@gmail.com>
2026-07-30 15:17:08 +0500

    Backend URL added

--- src/apis/axios.js
+++ src/apis/axios.js
@@ -1,8 +1,12 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://localhost:3000",
+  baseURL: import.meta.env.VITE_API_URL,
 });
+
+// const api = axios.create({
+//   baseURL: "http://localhost:3000",
+// });
 
 api.interceptors.request.use((config) => {
   const token = localStorage.getItem("token");
